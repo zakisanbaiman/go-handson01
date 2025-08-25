47b4b22b2ad293e95c708016caa34ded8ebd5816
zakisanbaiman <zakisanbaiman@gmail.com>
2025-08-25 09:43:16 +0900

    feat: 新しいルーティングライブラリ「chi」を追加し、タスクのリスト表示機能を実装しました。また、AddTaskHandlerの構造体名を変更し、テストケースを修正しました。

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 
 require (
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-chi/chi v1.5.5 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- mux.go
+++ mux.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-playground/validator/v10"
+	"github.com/zakisanbaiman/go-handson01/handler"
+	"github.com/zakisanbaiman/go-handson01/store"
 )
 
-func NewMux() *http.ServeMux {
-	mux := http.NewServeMux()
+// func NewMux() *http.ServeMux {
+// 	mux := http.NewServeMux()
+// 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+// 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+// 		_, _ = w.Write([]byte(`{"status": "ok"}`))
+// 	})
+// 	return mux
+// }
+
+func NewMux() http.Handler {
+	mux := chi.NewRouter()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+	v := validator.New()
+
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServeHTTP)
+
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
+
 	return mux
 }

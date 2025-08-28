814214fbdf60b071c212939ef06d2ae96aea8059
zakisanbaiman <zakisanbaiman@gmail.com>
2025-08-28 19:25:09 +0900

    feat: MySQLドライバーのバージョンを更新し、テスト用のデータベース接続機能を追加しました。また、タスク一覧取得のテストケースを実装しました。

--- go.mod
+++ go.mod
@@ -9,11 +9,13 @@ require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi v1.5.5
 	github.com/go-playground/validator/v10 v10.27.0
+	github.com/go-sql-driver/mysql v1.9.3
 	github.com/google/go-cmp v0.7.0
 	github.com/jmoiron/sqlx v1.4.0
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
---
+++ store/task_test.go
@@ -0,0 +1,91 @@
+package store
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/go-cmp/cmp"
+	"github.com/zakisanbaiman/go-handson01/entity"
+	"github.com/zakisanbaiman/go-handson01/testutil"
+)
+
+func TestRepository_ListTasks(t *testing.T) {
+	ctx := context.Background()
+
+	tx, err := testutil.OpenDBForTest(t).BeginTxx(ctx, nil)
+
+	t.Cleanup(func() {
+		_ = tx.Rollback()
+	})
+
+	if err != nil {
+		t.Fatalf("failed to begin tx: %s", err)
+	}
+
+	wants := prepareTasks(ctx, t, tx)
+
+	sut := &Repository{}
+	gots, err := sut.ListTasks(ctx, tx)
+	if err != nil {
+		t.Fatalf("failed to list tasks: %s", err)
+	}
+
+	if diff := cmp.Diff(wants, gots); diff != "" {
+		t.Errorf("ListTasks() mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func prepareTasks(ctx context.Context, t *testing.T, con Execer) entity.Tasks {
+	t.Helper()
+
+	if _, err := con.ExecContext(ctx, "DELETE FROM tasks"); err != nil {
+		t.Fatalf("failed to delete tasks: %s", err)
+	}
+
+	wants := entity.Tasks{
+		{
+			Title:      "test1",
+			Status:     entity.TaskStatusTodo,
+			CreatedAt:  time.Now(),
+			ModifiedAt: time.Now(),
+		},
+		{
+			Title:      "test2",
+			Status:     entity.TaskStatusDone,
+			CreatedAt:  time.Now(),
+			ModifiedAt: time.Now(),
+		},
+		{
+			Title:      "test3",
+			Status:     entity.TaskStatusTodo,
+			CreatedAt:  time.Now(),
+			ModifiedAt: time.Now(),
+		},
+	}
+
+	result, err := con.ExecContext(ctx,
+		`INSERT INTO tasks (title, status, created_at, modified_at) VALUES
+		(?, ?, ?, ?),
+		(?, ?, ?, ?),
+		(?, ?, ?, ?);`,
+		wants[0].Title, wants[0].Status, wants[0].CreatedAt, wants[0].ModifiedAt,
+		wants[1].Title, wants[1].Status, wants[1].CreatedAt, wants[1].ModifiedAt,
+		wants[2].Title, wants[2].Status, wants[2].CreatedAt, wants[2].ModifiedAt,
+	)
+	if err != nil {
+		t.Fatalf("failed to insert tasks: %s", err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		t.Fatalf("failed to get last insert id: %s", err)
+	}
+
+	wants[0].ID = entity.TaskID(id)
+	wants[1].ID = entity.TaskID(id + 1)
+	wants[2].ID = entity.TaskID(id + 2)
+
+	return wants
+}

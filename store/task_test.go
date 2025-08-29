6e219b46d3d4748705f75fcae8c4abdad9a5b901
zakisanbaiman <zakisanbaiman@gmail.com>
2025-08-29 09:10:32 +0900

    feat: go-sqlmockを追加し、RDBMSを使用しないタスク追加のテストケースを実装しました。また、go.modおよびgo.sumに依存関係を追加しました。

--- store/task_test.go
+++ store/task_test.go
@@ -5,12 +5,16 @@ import (
 	"testing"
 	"time"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/go-cmp/cmp"
+	"github.com/jmoiron/sqlx"
+	"github.com/zakisanbaiman/go-handson01/clock"
 	"github.com/zakisanbaiman/go-handson01/entity"
 	"github.com/zakisanbaiman/go-handson01/testutil"
 )
 
+// RDBMSを使ったテスト
 func TestRepository_ListTasks(t *testing.T) {
 	ctx := context.Background()
 
@@ -88,4 +92,38 @@ func prepareTasks(ctx context.Context, t *testing.T, con Execer) entity.Tasks {
 	wants[2].ID = entity.TaskID(id + 2)
 
 	return wants
+}
+
+// sqlmockを使ったテスト(RDBMSを使わない)
+func TestRepository_AddTask(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	c := clock.FixedClocker{}
+
+	var wantID int64 = 20
+
+	okTask := &entity.Task{
+		Title:      "ok test",
+		Status:     entity.TaskStatusTodo,
+		CreatedAt:  c.Now(),
+		ModifiedAt: c.Now(),
+	}
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { _ = db.Close() })
+
+	mock.ExpectExec("INSERT INTO tasks \\(title, status, created_at, modified_at\\) VALUES \\(\\?, \\?, \\?, \\?\\);").
+		WithArgs(okTask.Title, okTask.Status, okTask.CreatedAt, okTask.ModifiedAt).
+		WillReturnResult(sqlmock.NewResult(wantID, 1))
+
+	xdb := sqlx.NewDb(db, "mysql")
+	r := &Repository{Clocker: c}
+	if err := r.AddTask(ctx, xdb, okTask); err != nil {
+		t.Errorf("failed to add task: %s", err)
+	}
 }

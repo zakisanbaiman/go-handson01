108f1c1c562d3aa3275f45e59fce0a62e5db7d7e
zakisanbaiman <zakisanbaiman@gmail.com>
2025-10-04 09:04:11 +0900

    feat: add middlwware

---
+++ handler/middleware.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zakisanbaiman/go-handson01/auth"
+)
+
+func AuthMiddleware(j *auth.JWTer) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			req, err := j.FillContext(r)
+			if err != nil {
+				RespondJSON(r.Context(), w, ErrResponse{
+					Message: "not find auth infor",
+					Details: []string{err.Error()},
+				}, http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, req)
+		})
+	}
+}
+
+func AdminMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !auth.IsAdmin(r.Context()) {
+			RespondJSON(r.Context(), w, ErrResponse{
+				Message: "not admin",
+			}, http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
